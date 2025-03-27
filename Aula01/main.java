cde5612a49864ae328496b9b64338935790d67ba
Alec Campos Aoki <alec.campsaoki@gmail.com>
2025-03-27 14:39:08 -0300

    Update main.java, add pdf

--- Aula01/main.java
+++ Aula01/main.java
@@ -2,5 +2,9 @@ public class main{
     static public void main(String args[]){
         Cadeira c1 = new Cadeira();
         Cadeira c2 = new Cadeira("Virado", false);
+        c1.sentar();
+        c2.virar();
+        System.out.println(c1.getPosicao());
+        System.out.println(c2.getPosicao());
     }
 }
