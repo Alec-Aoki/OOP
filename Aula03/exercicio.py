df6eb2a1e3cbbdc8a932a429698ba0ce8f335d7f
Alec Campos Aoki <alec.campsaoki@gmail.com>
2025-04-10 13:44:51 -0300

    Finish exercicio

--- Aula03/exercicio.py
+++ Aula03/exercicio.py
@@ -11,6 +11,8 @@ class Tabuleiro:
 
     def __init__(self, seqNum, tamanho):
         tamSqr = int(math.sqrt(tamanho)) # Quantidade de colunas e fileiras que o tabuleiro terá
+        self.tamSqr = tamSqr
+
         self.tabuleiro = [[0 for _ in range(tamSqr)] for _ in range(tamSqr)] # Criando a matriz tamSqr x tamSqr
 
         # Preenchendo o tabuleiro
@@ -20,7 +22,64 @@ class Tabuleiro:
                 self.tabuleiro[i][j] = seqNum[k]
                 k += 1
 
+    # Retorna as coordenadas i e j da posição do 0 dentro do tabuleiro
+    def encontrarZero(self):
+        for i in range(self.tamSqr):
+            for j in range(self.tamSqr):
+                if(self.tabuleiro[i][j] == 0):
+                    return i, j
+                
+    # Funções de movimento:
+    def direita(self):
+        i, j = self.encontrarZero()
+        if(j != 0):
+            # Possível mover a peça pra direita, trocar os valores
+            self.tabuleiro[i][j] = self.tabuleiro[i][j - 1]
+            self.tabuleiro[i][j - 1] = 0
 
+    def esquerda(self):
+        i, j = self.encontrarZero()
+        if(j != self.tamSqr - 1):
+            # Possível mover a peça pra direita, trocar os valores
+            self.tabuleiro[i][j] = self.tabuleiro[i][j + 1]
+            self.tabuleiro[i][j + 1] = 0
+
+    def cima(self):
+        i, j = self.encontrarZero()
+        if(i != self.tamSqr - 1):
+            # Possível mover a peça pra direita, trocar os valores
+            self.tabuleiro[i][j] = self.tabuleiro[i + 1][j]
+            self.tabuleiro[i + 1][j] = 0
+
+    def baixo(self):
+        i, j = self.encontrarZero()
+        if(i != 0):
+            # Possível mover a peça pra direita, trocar os valores
+            self.tabuleiro[i][j] = self.tabuleiro[i - 1][j]
+            self.tabuleiro[i - 1][j] = 0
+
+    def tabuleiroCorreto(self):
+        k = 0
+        for i in range(self.tamSqr):
+            for j in range(self.tamSqr):
+                if(self.tabuleiro[i][j] != k):
+                    return False
+                k += 1
+
+        return True
+    
+    def printarTabuleiro(self):
+        linhaHoriz = "+" + "------+" * self.tamSqr
+        print(linhaHoriz)
+        for i in range(self.tamSqr):
+            linha = "|"
+            for j in range(self.tamSqr):
+                if(self.tabuleiro[i][j] == 0):
+                    linha += f"      |"
+                else:
+                    linha += f"  {self.tabuleiro[i][j]:2d}  |"
+            print(linha)
+            print(linhaHoriz)
 
 def main():
     # Lê a sequência de inteiros digitada pelo usuário como uma string, separa nos espaços, transforma em inteiros e transforma em uma lista
@@ -29,7 +88,28 @@ def main():
 
     tab = Tabuleiro(seqEntrada, tamEntrada) # Criando objeto tabuleiro
 
-    print(tab.tabuleiro)
+    tab.printarTabuleiro() # Printando tabuleiro inicial
+    print("")
+
+    movimentos = list(input()) # Pegando string de movimentos e transformando em uma lista de chars
+
+    # Executando os movimentos digitados
+    for i in range(len(movimentos)):
+        mov = movimentos[i]
+        if(mov == 'd'):
+            tab.baixo()
+        elif(mov == 'u'):
+            tab.cima()
+        elif(mov == 'l'):
+            tab.esquerda()
+        elif(mov == 'r'):
+            tab.direita()
+
+        tab.printarTabuleiro() # Printa o tabuleiro após cada movimento
+        print("")
+
+
+    print(f"Posicao final: {tab.tabuleiroCorreto()}")
 
 if __name__ == "__main__":
     main()
